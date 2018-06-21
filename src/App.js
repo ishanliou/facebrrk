c77757302aa1592b80b388b755811d47a55310f1
Ishan <ishanliou@yahoo.com>
2018-06-20 23:27:45 -0700

    barks details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 import Home from './components/Home';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = '5daa8b029202d6ddfe64a61bbab96ce1'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
     render() {
-        retrun(
-
+        return(
+            <input className="search-bar" placeholder="search a pet..."/>
         )
     }
         
