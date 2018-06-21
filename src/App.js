8499586c307340b865e987d4e0d1afe4c0571fd1
Ishan <ishanliou@yahoo.com>
2018-06-21 12:57:09 -0700

    petfinder- client

--- src/App.js
+++ src/App.js
@@ -4,13 +4,39 @@ import './App.css'
 
 import Home from './components/Home';
 import SearchBar from './components/SearchBar';
+import petfinder from './petfinder-client'
+import credentials from './credentials'
 
-const API_KEY = '5daa8b029202d6ddfe64a61bbab96ce1'
+const petFinderKey = petfinder(credentials)
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      animal: 'dog',
+      breed: 'Havanese',
+      location: 'San Francisco, CA',
+      pets: []
+    }
+  }
+
+  componentDidMount() {
+    const { animal, breed, location } = this.state
+    const promise = petFinderKey.pet.find({animal, breed, location, output: 'full'})
+    promise.then((data) => {
+      const pets =data.petfinder.pets ? data.petfinder.pets.pet : []
+      this.setState({pets})
+    })
+  }
+
   render() {
     return (
       <div className="App">
+      {this.state.pets.map((pet => {
+        <h1>{pet.name}</h1>
+        {JSON.stringify(this.state, null, 4)}
+      }))}
         <Home />
         <SearchBar />
       </div>
---
+++ src/credentials.js
@@ -0,0 +1,4 @@
+export default {
+    key: "5daa8b029202d6ddfe64a61bbab96ce1",
+    secret: "707fd3f2d229eafcc5f93a56f43ffe63"
+}
