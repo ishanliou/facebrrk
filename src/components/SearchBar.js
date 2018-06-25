e5e1f13a68cbcd4408e91f349c391dfd2fb86bff
Ishan <ishanliou@yahoo.com>
2018-06-25 15:35:21 -0700

    input value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,8 @@ const pf = petfinder()
 
 class SearchBar extends Component {
     state = {
-        breeds: []
+        breeds: [],
+        value: ""
     }
 
     componentDidMount () {
@@ -20,9 +21,11 @@ class SearchBar extends Component {
           })
       }
 
-    searchBreed(e) {
-        console.log('yay')
+    updateBreed(e) {
+        console.log(e.target.value)
+        this.setState({value: e.target.value})
     }
+
     render() {
         return(
             <div>
@@ -30,12 +33,12 @@ class SearchBar extends Component {
                 <input className="search-bar" 
                        type="text" 
                        list="breed-list"
-                       placeholder="search a pet..." 
-                       onChange={this.searchBreed.bind(this)}/>
+                       placeholder="search a breed..." 
+                       onChange={this.updateBreed.bind(this)}/>
                 <datalist id="breed-list">
-                    {this.state.breeds.map((breed) => 
-                        <option key={breed} value={breed} />
-                    )}
+                        {this.state.breeds.map((breed) => 
+                            <option key={breed} value={breed}/>
+                        )}
                     
                 </datalist>
 
