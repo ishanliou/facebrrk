ed755bc4fff9f1085fc50411e0abcce7ee487502
Ishan <ishanliou@yahoo.com>
2018-07-16 17:13:32 -0700

    about imgs

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,20 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <img src={`hero-img.png`} style={{width:'1150px', height:'auto' }} alt="about-img" />
+            <div className="about-img">
+            <img src={`hero-img-sm.png`} style={{width:'1150px', height:'auto' }} alt="about-img" />
+            </div>
+            <div className="about-img">
+            <img src={`FaceBrrk-about-bark02.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk02" />
+            </div>
+            <div className="about-img">
+            <img src={`FaceBrrk-about-bark03.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk03" />
+            </div>
+            <div className="about-img">
+            <img src={`FaceBrrk-about-bark04.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk04" />
+            </div>
         </div>
     )
-
 }
 
 export default About
