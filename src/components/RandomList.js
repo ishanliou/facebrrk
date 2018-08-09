2f2159574e5db03380aabd7b6eb17fc22b50d8ea
Ishan <ishanliou@yahoo.com>
2018-08-09 14:57:27 -0700

    random breed info

--- src/components/RandomList.js
+++ src/components/RandomList.js
@@ -7,16 +7,29 @@ class RandomList extends Component{
         // console.log('ooo', (Object.values(randomPets)[0]))
         console.log('rrr', (Object.entries(randomPets)[0]))
        console.log('therer', randomPets)
-       
+       console.log('breed', (randomPets.breeds))
+       const breed = randomPets.breeds? randomPets.breeds: 'n/a'
+       console.log('contact', randomPets.contact)
+       const contact = randomPets.contact? randomPets.contact : 'n/a'
+       console.log("photo", randomPets.media)
         return(
             <div>
                 <div>Name: {randomPets.name}</div>
                 <div>Age: {randomPets.age}</div>
                 <div>Sex: {randomPets.sex}</div>
                 <div>Size: {randomPets.size}</div>
-                {/* <div>{randomPets.breeds.breed[0]} | {randomPets.breeds.breed[1]}</div> */}
-                {/* <div>{randomPets.contact}</div> */}
-                
+                <div>Breed: {Object.values(breed).map( e => e )}</div>
+                <div>Contact: 
+                  <ul>City: {contact.city}</ul>
+                  <ul>State:{contact.state}</ul>
+                  <ul>ZipCode: {contact.zip}</ul>
+                  <ul>Email: {contact.email}</ul>
+                  <ul>Phone: {contact.phone}</ul>
+                  
+                  </div>
+                  
+                <div>Shelter Pet ID: {randomPets.shelterPetId ? randomPets.shelterPetId: "n/a"}</div>
+
                 <div>Shelter ID: {randomPets.shelterId}</div>
                 {/* <div>{randomPets.media.photos.photo[2].value}</div> */}
 
