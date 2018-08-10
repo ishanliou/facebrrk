1fcd29d43053b49cca7beeb39e527a73018036d5
Ishan <ishanliou@yahoo.com>
2018-08-10 15:31:03 -0700

    select tag

--- src/components/RandomBark.js
+++ src/components/RandomBark.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class RandomBark extends Component {
 
     updateRandom(e) {
-        console.log('yay')
+        console.log('yay',e.target.value)
         this.props.getRandomPet(e.target.value)
     }
 
@@ -11,26 +12,31 @@ class RandomBark extends Component {
         return(
             <div className="search-field">
                 <label className="label">Match: </label>
-                <input className="search-bar"
+                
+                {/* <input className="search-bar"
                        type="text"
                        list="zodiac-list"
                        placeholder="Match your zodiac sign..."
                        onChange={this.updateRandom.bind(this)}/>
-
-                <datalist id="zodiac-list">
-                    <option value="Aquarius"/>
-                    <option value="Pisces"/>
-                    <option value="Aries"/>
-                    <option value="Taurus"/>
-                    <option value="Gemini"/>
-                    <option value="Cancer"/>
-                    <option value="Leo"/>
-                    <option value="Virgo"/>
-                    <option value="Libra"/>
-                    <option value="Scorpio"/>
-                    <option value="Sagittarius"/>
-                    <option value="Capricorn"/>
-                </datalist>
+                */}
+               <Link to={`/findbrrk/random`}>
+                <select id="zodiac-list" onChange={this.updateRandom.bind(this)}>
+                    <option >Match your zodiac sign</option>
+                    <option value="Aquarius">Aquarius</option>
+                    <option value="Pisces">Pisces</option>
+                    <option value="Aries">Aries</option>
+                    <option value="Taurus">Taurus</option>
+                    <option value="Gemini">Gemini</option>
+                    <option value="Cancer">Cancer</option>
+                    <option value="Leo">Leo</option>
+                    <option value="Virgo">Virgo</option>
+                    <option value="Libra">Libra</option>
+                    <option value="Scorpio">Scorpio</option>
+                    <option value="Sagittarius">Sagittarius</option>
+                    <option value="Capricorn">Capricorn</option>
+                   
+                </select>
+                </Link>
                 
             </div>
 
