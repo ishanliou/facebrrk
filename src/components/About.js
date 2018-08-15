cfbfb8ec3bca355fc781cf968641dc7f71a82df6
Ishan <ishanliou@yahoo.com>
2018-08-15 15:49:27 -0700

    about imgs

--- src/components/About.js
+++ src/components/About.js
@@ -6,21 +6,7 @@ const About = () => {
             <div className="about-img">
             <img src={`hero-img-sm.png`} style={{width:'1150px', height:'auto' }} alt="about-img" />
             </div>
-            <div className="about-img">
-            <img src={`FaceBrrk-about-bark02.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk02" />
-            </div>
-            <div className="about-img">
-            <img src={`FaceBrrk-about-bark03.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk03" />
-            </div>
-            <div className="about-img">
-            <img src={`FaceBrrk-about-bark04.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk04" />
-            </div>
-            <div className="about-img">
-            <img src={`FaceBrrk-about-bark05.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk05" />
-            </div>
-            <div className="about-img">
-            <img src={`FaceBrrk-about-bark06.png`} style={{width:'1150px', height:'auto' }} alt="about-brrk06" />
-            </div>
+           
         </div>
     )
 }
