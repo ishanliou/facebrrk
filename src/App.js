44643f23bbbbdbbb5350cf94d355e12c49447dfa
Ishan <ishanliou@yahoo.com>
2018-08-16 16:43:33 -0700

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Home from './components/Home'
+import Home from './components/Home/Home'
 import BarkList from './components/BarkList'
 import SearchBar from './components/SearchBar'
 import SearchLocation from './components/SearchLocation'
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+        return(
+            <div className="home">
+              <div className="item">
+                <img src={`FaceBrrk-about-03.png`}  alt="hero-img"/>
+
+              </div>
+            </div>
+        )
+}
+
+export default Home;
