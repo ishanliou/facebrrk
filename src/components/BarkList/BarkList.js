84f4db0537ea709d9e5c3143c9614147a4bab5e2
Ishan <ishanliou@yahoo.com>
2018-08-22 14:47:00 -0700

    text color

--- src/components/BarkList/BarkList.js
+++ src/components/BarkList/BarkList.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 import './barklist.css'
 
 
-class BarkList extends Component {   
+class BarkList extends Component {  
   render(){
     const bark = this.props.bark
     return(
       <div className="bark-lists">
         {bark.map((pet => (
-      <div className="card">
+      <div className="card" key={pet.id}>
         <Link to={`/findbrrk/${pet.id}`}>
-          <img className="card-img-top" src={pet.media.photos? pet.media.photos.photo[2].value: null} alt={pet.name}/>
+          <img className="card-img-top" src={pet.media.photos? pet.media.photos.photo[2].value: ''} alt={pet.name}/>
         </Link>
      
         <div className="card-body">
           <h5 className="card-title">{pet.name}</h5>
           <h6 className="card-text"><span className="sub-title">Age:</span> {pet.age}</h6>
           <h6 className="card-text"><span className="sub-title">Sex:</span> {pet.sex}</h6>
-          {/* <p className="card-text">Breed: {pet.breeds.breed}</p> */}
+          <h6 className="card-text"><span className="sub-title">Breed:</span> {pet.breeds.breed}</h6>
           <h6 className="card-text"><span className="sub-title">Location:</span> {pet.contact.city}, {pet.contact.state} {pet.contact.zip}</h6>
           {/* <p className="card-text">Description: {pet.description}</p> */}
         </div>
