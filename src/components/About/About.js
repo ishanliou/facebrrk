e228dad0234dbd314d6ddf9f2050fddbaebf9c69
Ishan <ishanliou@yahoo.com>
2018-09-07 16:27:20 -0700

    about home img

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './about.css'
 
 const About = () => {
 	return (
-		<div>
+		<div className="about-container">
 			<div className="about-img">
-			 <img src={`hero-img-sm.png`} style={{width:'1150px', height:'auto' }} alt="about-img" />
+			 <img src={`hero-img-sm.png`} alt="about-img" />
 			</div>
 				
 		</div>
