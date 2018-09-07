ab62620e390c9f32b0e7024e53c1cc78ea1f93eb
Ishan <ishanliou@yahoo.com>
2018-09-07 14:38:04 -0700

    nav bar font

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,14 +4,15 @@ import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="nav-bar">
-        <div className="tag-line">Love is where the brrk is</div>
+    <div className="nav-bar">
+      <div className="tag-line">Love is where the brrk is</div>
+      <div className="menu">
         <ul>
           <li><Link to="/home" > Home </Link></li>
           <li><Link to="/about" > About </Link></li>
           <li><Link to="/findbrrk" > Find Brrk </Link></li>
         </ul>
+        
       </div>
     </div>
   )
