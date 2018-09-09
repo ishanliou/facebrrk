83a63e3aebee6044042a0babb58a24d3656166c3
Ishan <ishanliou@yahoo.com>
2018-09-09 00:02:39 -0700

    description layout

--- src/components/BarkDetails/BarkDetails.js
+++ src/components/BarkDetails/BarkDetails.js
@@ -36,17 +36,48 @@ class BarkDetsils extends Component {
     const thisPet = this.state.selectedPet
     return(
       <div className="bark-details">
-        <div className="card bark-details-card">
-        <img className="card-img-top bark-details-img" src={this.state.media} alt={thisPet.name}/>
-        <div className="card-body bark-details-card-body">
-          <h5 className="card-title"><span className="sub-title">Name:</span> {thisPet.name}</h5>
-          <h6 className="card-text"><span className="sub-title">Breed:</span> {this.state.petBreed[0]} | {this.state.petBreed[1]}</h6>
-          <h6 className="card-text"><span className="sub-title">Age:</span> {thisPet.age}</h6>
-          <h6 className="card-text"><span className="sub-title">Id:</span> {thisPet.id}</h6>
-          <h6 className="card-text"><span className="sub-title">Gender:</span> {thisPet.sex}</h6>
-          <h6 className="card-text"><span className="sub-title">Size:</span> {thisPet.size}</h6>
-          <h6 className="card-text description"><span className="sub-title">Description:</span> {thisPet.description}</h6>
-          <button type="button" className="btn btn-lg btn-dark" onClick={this.goBack.bind(this)}>Go Back </button>
+        <div className="bark-details-card">
+          <div>
+          <img className="bark-details-card-img-top" src={this.state.media} alt={thisPet.name}/>
+          </div>
+        <div className="bark-details-card-body">
+          <div className="card-title">
+            <span className="sub-title"></span> 
+              {thisPet.name}
+          </div>
+
+          <div className="card-text">
+            <span className="sub-title">Breed:</span> 
+            {this.state.petBreed[0]} | {this.state.petBreed[1]}
+          </div>
+
+          <div className="card-text">
+            <span className="sub-title">Age:</span> 
+              {thisPet.age}
+          </div>
+
+          <div className="card-text">
+            <span className="sub-title">Id:</span> 
+              {thisPet.id}
+          </div>
+
+          <div className="card-text">
+            <span className="sub-title">Gender:</span> 
+              {thisPet.sex}
+          </div>
+
+          <div className="card-text">
+            <span className="sub-title">Size:</span> 
+              {thisPet.size}
+          </div>
+
+          <div className="card-text text-description">
+            <span className="sub-title">Description:</span> 
+            {thisPet.description}
+          </div>
+         
+            <button type="button" className="btn-go-back" onClick={this.goBack.bind(this)}>Go Back </button>
+        
         </div>
       </div>      
       </div>
