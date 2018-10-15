48a3c33342c7216da540739877fa7977091bb52c
Ishan <ishanliou@yahoo.com>
2018-10-14 22:59:25 -0700

    responsive nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
-const Navbar = () => {
-  return (
-    <div>
-      {/* For wide screen */}
-      <div className="nav-bar">
-        <div className="tag-line">
-          <Link to="/home">
-            <img src={`./image/facebrrk-logo.svg`} style={{width:"50px", height:"50px", paddingRight:"1.5em", paddingTop:"1em"}} alt="logo"/>
-          </Link>
-          <Link to="/home">
-          <h1>FaceBrrk</h1>
-          </Link>
-        </div>
-        <div className="wide-nav-menu">
-          <ul>
-            {/* <li><Link to="/home" > Home </Link></li> */}
-            <li><Link to="/match" > Find Your Match </Link></li>
-            <li><Link to="/findbrrk" > Find Brrk </Link></li>
-            <li><Link to="/about" > About </Link></li>                    
-          </ul>
-        </div>
-       </div>
+class Navbar extends Component {
+  constructor () {
+    super();
+    this.state = {
+      showMenu: false,
+  }
+  this.showMenu = this.showMenu.bind(this);
+  this.closeMenu = this.closeMenu.bind(this);
+  }
 
-      {/* For narrow screen */}
-      <div className="nav-bar-narrow">
-        <i className="fa fa-bars fa-3x"></i>
-        
-        <div className="narrow-nav-menu">
-          <ul>
-            {/* <li><Link to="/home" > Home </Link></li> */}
-            <li><Link to="/match" > Find Your Match </Link></li>
-            <li><Link to="/findbrrk" > Find Brrk </Link></li>
-            <li><Link to="/about" > About </Link></li>                    
-          </ul>
-        </div>
-       </div>
+  showMenu (e) {
+    e.preventDefault();
 
+    this.setState({showMenu: true}, () => {
+      document.addEventListener('click', this.closeMenu)
+    })
+  }
 
-      </div>
-  )
+  closeMenu () {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu)
+    })
+  }
+
+  render() {
+    return (
+      <div className="navbar-container">
+        {/* For wide screen */}
+        <div className="nav-bar">
+          <div className="tag-line">
+            <Link to="/home">
+              <img src={`./image/facebrrk-logo.svg`} style={{width:"50px", height:"50px", paddingRight:"1.5em", paddingTop:"1em"}} alt="logo"/>
+            </Link>
+            <Link to="/home">
+            <h1>FaceBrrk</h1>
+            </Link>
+          </div>
+          <div className="wide-nav-menu">
+            <ul>
+              <li><Link to="/match" > Find Your Match </Link></li>
+              <li><Link to="/findbrrk" > Find YourBrrk </Link></li>
+              <li><Link to="/about" > About </Link></li>                    
+            </ul>
+          </div>
+         </div>
+  
+        {/* For narrow screen */}
+        <div className="nav-bar-narrow">
+          <i className="fa fa-bars fa-4x" onClick={this.showMenu}></i>
+          {
+            this.state.showMenu
+            ?(
+              <div className="narrow-nav-menu">
+            <ul>
+              <li><Link to="/match" > Find Your Match </Link></li>
+              <li><Link to="/findbrrk" > Find Your Brrk </Link></li>
+              <li><Link to="/about" > About </Link></li>                    
+            </ul>
+          </div>
+            )
+            :(
+              null
+            )
+          }
+          
+         </div>
+         </div>
+    )
+  }
 }
 
 export default Navbar
